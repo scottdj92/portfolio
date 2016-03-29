f3f4097434284fc870cebb0050e89430b9d523a6
Scott Jones <scottdj92@gmail.com>
2016-03-29 14:04:37 -0400

    added routing

--- app/app.js
+++ app/app.js
@@ -4,7 +4,27 @@ var MainController = require('./controllers/MainController');
 var WorkController = require('./controllers/WorkController');
 var ContactController = require('./controllers/ContactController');
 
-var app = angular.module('app', []);
+
+var app = angular.module('app', [require('angular-route')]);
 app.controller('MainController', ['$scope', MainController]);
 app.controller('WorkController', ['$scope', WorkController]);
 app.controller('ContactController', ['$scope', ContactController]);
+
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'partials/main.html',
+			controller: 'MainController'
+		}).
+		when('/work', {
+			templateUrl: 'partials/work.html',
+			controller: 'WorkController'
+		}).
+		when('/about:workId', {
+			templateUrl: 'partials/work-detail.html',
+			controller: 'WorkDetailController'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	}]);
--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -1,4 +1,8 @@
 module.exports = function($scope) {
 	$scope.message = "Hi I'm Scott Jones";
 	$scope.subtitle = "I'm an interactive developer.";
+	$scope.mainMessage = 'Work';
+	$scope.subtitleMessage = 'Some of my work';
+	$scope.contactLine = "I'm Scott, but you can call me";
+	$scope.contactHook = 'anytime';
 };

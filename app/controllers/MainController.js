5d400fc3de35f294a8440ff5026dbfa37ff72cd0
Scott Jones <scottdj92@gmail.com>
2016-03-29 17:09:18 -0400

    updated mainController

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -6,6 +6,32 @@ module.exports = function($scope) {
 	$scope.contactLine = "I'm Scott, but you can call me";
 	$scope.contactHook = 'anytime.';
 
+	$scope.aboutHook = 'Who am I?';
+	$scope.bio = '';
+
+	$scope.skills = [
+	  {
+	    "name": "Backbone.js",
+	    "src": "https://upload.wikimedia.org/wikipedia/commons/2/20/Backbone.js_logo.svg"
+	  },
+	  {
+	  	"name": "Angular.js",
+	  	"src": "https://raw.githubusercontent.com/angular/angular.js/master/images/logo/AngularJS-Shield.exports/AngularJS-Shield-small.png"
+	  },
+	  {
+	  	"name": "Adobe Creative Suite",
+	  	"src": "https://worldvectorlogo.com/logos/creative-cloud-cc.svg"
+	  },
+	  {
+	  	"name": "WordPress",
+	  	"src": ""
+	  },
+	  {
+	  	"name": "",
+	  	"src": ""
+	  }
+	]
+
 	$scope.master = {};
 
 	$scope.reset = function(form) {
